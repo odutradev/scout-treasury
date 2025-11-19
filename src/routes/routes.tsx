1c4f7da465e9c138163a65b0bd4b5bcdf0cea840
odutradev <joao.vitornl@gmail.com>
2025-11-19 17:35:51 -0300

    chore: Updating .env typing

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -2,7 +2,7 @@ import { Navigate } from 'react-router-dom';
 
 import Dashboard from '@pages/dashboard';
 import NotFound from '@pages/notFound';
-import SignIn from '@pages/signin';
+import SignIn from '@pages/signIn';
 import Home from '@pages/home';
 
 const routes = [
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -4,6 +4,8 @@ interface ImportMetaEnv {
     readonly VITE_CONTROL_ACCESS: string;
     readonly VITE_PRODUCTION: string;
     readonly VITE_BASEURL: string;
+    readonly VITE_PIN_NORMAL: string;
+    readonly VITE_PIN_ADMIN: string;
 }
   
 interface ImportMeta {
